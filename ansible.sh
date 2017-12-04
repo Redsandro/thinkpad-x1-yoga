467c2f0f4608b324eb53e2ad4c412330e787b801
Sander Steenhuis <redsandro@gmail.com>
2017-12-04 15:11:45 +0100

    Update firefox; Fix repo for avidemux

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-
+#
 # For updating tlp settings, run:
 # sudo ./ansible.sh x1yoga.yml --tags tlp
+#
+# For remote provisioning, set up:
+# /etc/ansible/hosts
+# env ANSIBLE_NOCOWS=1 ansible-playbook --ask-become-pass --skip-tags="hidpi,yoga,oled" x1remote.yml
 
 SU=""
 if [ "$(whoami)" != "root" ]; then
